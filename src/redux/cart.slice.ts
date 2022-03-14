8eb6b0d94fc63a23c1efa822e564a5853633069b
Gustavo Ferreira da Silva <gustavofersilva@gmail.com>
2022-03-14 17:26:23 -0300

    Corrigido erro de tipagem da Store do Redux

--- src/redux/cart.slice.ts
+++ src/redux/cart.slice.ts
@@ -1,34 +1,45 @@
 import { createSlice } from '@reduxjs/toolkit'
+import { RootStateOrAny } from 'react-redux'
 import { Reducer } from 'redux'
 
 const cartSlice = createSlice({
   name: 'cart',
   initialState: [],
   reducers: {
-    addToCart: (state, action) => {
-      const itemExists = state.find((item) => item.id === action.payload.id)
+    addToCart: (state: RootStateOrAny, action) => {
+      const itemExists = state.find(
+        (item: RootStateOrAny) => item.id === action.payload.id
+      )
       if (itemExists) {
         itemExists.quantity++
       } else {
         state.push({ ...action.payload, quantity: 1 })
       }
     },
-    incrementQuantity: (state, action) => {
-      const item = state.find((item) => item.id === action.payload)
+    incrementQuantity: (state: RootStateOrAny, action) => {
+      const item = state.find(
+        (item: RootStateOrAny) => item.id === action.payload
+      )
       item.quantity++
     },
 
     decrementQuantity: (state, action) => {
-      const item = state.find((item) => item.id === action.payload)
+      const item: RootStateOrAny = state.find(
+        (item: RootStateOrAny) => item.id === action.payload
+      )
       if (item.quantity === 1) {
-        const index = state.findIndex((item) => item.id === action.payload)
+        const index = state.findIndex(
+          (item: RootStateOrAny) => item.id === action.payload
+        )
         state.splice(index, 1)
       } else {
         item.quantity--
       }
     },
     removeFromCart: (state, action) => {
-      const index = state.findIndex((item) => item.id === action.payload)
+      const index = state.findIndex(
+        (item: RootStateOrAny) => item.id === action.payload
+      )
       state.splice(index, 1)
     },
   },
